a7dd4ee8f22bac634207330f632bde37fae2e6ee
JulianKropp <JulianKropp@users.noreply.github.com>
2023-11-18 23:46:38 +0100

    💡 Remove explanation

--- main.go
+++ main.go
@@ -7,118 +7,6 @@ import (
 	"time"
 )
 
-// Explanation of the code:
-// ssePubSub is Handler which send live data to the client using SSE.
-// The client receves a json string over SSE which looks like this:
-// {
-// 	"sys":[
-// 	   {
-// 		  "type":"topics",
-// 		  "list":[
-// 			 {
-// 				"name":"server/status",
-// 				"type":"public"
-// 			 },
-// 			 {
-// 				"name":"test/server",
-// 				"type":"private"
-// 			 },
-// 			 {
-// 				"name":"test/server2",
-// 				"type":"private"
-// 			 }
-// 		  ]
-// 	   },
-// 	   {
-// 		  "type":"subscribed",
-// 		  "list":[
-// 			 {
-// 				"name":"server/status"
-// 			 },
-// 			 {
-// 				"name":"test/server"
-// 			 }
-// 		  ]
-// 	   },
-// 	   {
-// 		  "type":"unsubscribed",
-// 		  "list":[
-// 			 {
-// 				"name":"test5/server"
-// 			 }
-// 		  ]
-// 	   }
-// 	],
-// 	"updates":[
-// 	   {
-// 		  "topic":"server/status",
-// 		  "data":{
-// 			 "testdata":"testdata"
-// 		  }
-// 	   }
-// 	]
-//  }
-
-// Empty example:
-// {
-// 	"sys": null,
-// 	"updates": null
-// }
-
-// Data update example:
-// {
-// 	"sys": null,
-// 	"updates":[
-// 	   {
-// 		  "topic":"topic",
-// 		  "data":{
-//			 "testdata":"testdata"
-// 		  }
-// 	   }
-// 	]
-// }
-
-// There are 3 types of topics:
-// PUBLIC: All clients can subscribed to this topic. Data send in this topic will be send to all clients.
-// PRIVATE: Only thhis client can subscribed to this topic. Data send in this topic will be send to only this client
-// GROUP: All clients in the same group can subscribed to this topic. Data send in this topic will be send to all clients in the same group.
-
-// Topics are case sensetive and are limited to the alphabet, numbers, underscores.
-// Topics can have sub topics seperated by a slash.
-// Subtopics can have subtopics too.
-// Example: topic1/test/test
-// If you subscribe to a topic, you will also subscribe to all subtopics.
-// Example: If you subscribe to topic1, you will also subscribe to topic1/test and topic1/test/test, ...
-// Example: If you subscribe to topic1/test, you will also subscribe to topic1/test/test, ...
-
-// if somehow multiple topics with the same name of different types exists, the order of the types is:
-// 1. PRIVATE
-// 2. GROUP
-// 3. PUBLIC
-// This ensures PRIVATE and GROUP topics can not be overridden and made public exededently.
-
-// sys.topics: Event: List of all public, group and private topics
-// sys.subscribed: Event: List of all public, group and private topics the client is subscribed to
-// sys.unsubscribed: Event: List of all public, group and private topics the client is no longer subscribed to
-// updates: List of all public, group and private topics the client is subscribed to and which have been updated with new data
-
-// Only changes will be send to the client:
-
-// If a topic is added, the hole sys.list will be send to the client
-
-// If a topic is removed, the hole new sys.list will be send to the client, and
-// only this topic which was removed will be send in sys.unsubsribed
-
-// If a client is subscribed to a topic, only the topic will be send to the client in sys.subscribed.
-
-// If a client is unsubscribed from a topic, only the topic will be send to the client in sys.unsubscribed.
-
-// If there is a new update for a topic, only this topic will be send to the client in updates.
-
-type TestData struct {
-	Testdata string `json:"testdata"`
-}
-
 func main() {
 	// Create a new SSEPubSubService
 	ssePubSub := NewSSEPubSubService()
@@ -219,4 +107,8 @@ func main() {
 	ssePubSub.RemoveClient(client)
 
 	time.Sleep(500 * time.Second)
+}
+
+type TestData struct {
+	Testdata string `json:"testdata"`
 }
